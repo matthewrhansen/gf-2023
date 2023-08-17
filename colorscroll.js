726876cdd40233f41df672a03d6d76f24e2ba053
Matthew <matthew.hansen9@gmail.com>
2023-08-17 16:08:18 -0400

    Create colorscroll.js

---
+++ colorscroll.js
@@ -0,0 +1,3 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/lumosframework/scripts@v1.0.3/colorscroll.js"></script>
